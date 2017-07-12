ff8fab84fc0ed84b0aace221786e99daebd528bf
Simon Cropp <simon.cropp@gmail.com>
2017-07-12 13:21:55 +1000

    fix assembly info

--- NServiceBus.Bond/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("NServiceBus.Bond")]
-[assembly: AssemblyProduct("NServiceBus.Bond")]
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
